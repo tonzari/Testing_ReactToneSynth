c166904716cfe4b59fa6d187d4821fd0db041be1
Antonio Manzari <antoniomanzari@gmail.com>
2022-06-28 23:16:45 -0400

    mutliple synth instances tried. works!

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
 import * as Tone from "tone";
 import { useRef } from "react";
 
-const synth = new Tone.PolySynth().toDestination();
-let freqs = [30,60,90,120];
-let num = 2;
-
-function playSynth() {
-  synth.triggerAttackRelease(freqs, 0.01)
-  increaseFreqs();
-}
-
-function increaseFreqs() {
-  freqs = freqs.map(f =>  f + +num);
-  console.log(freqs)
-}
-
-function resetFreqs() {
-  freqs = [30,60,90,120];
-}
-
 function App() {
+  const synth = new Tone.PolySynth().toDestination();
+  let freqs = [20, 120, 220, 320];
+  let num = 49;
 
+  function playSynth() {
+    synth.triggerAttackRelease(freqs, 0.01);
+    increaseFreqs();
+  }
+
+  function increaseFreqs() {
+    freqs = freqs.map((f) => f + +num);
+    console.log(freqs);
+  }
+
+  function resetFreqs() {
+    freqs = [20, 120, 220, 320];
+  }
   const numberRef = useRef();
 
   function handleChangeNumber(e) {
-     num = numberRef.current.value;
-     console.log(num)
+    num = numberRef.current.value;
+    console.log(num);
   }
 
   return (
     <div>
-      <button id="button" onClick={ playSynth }>click for synth note</button>
-      <button onClick={ resetFreqs }>reset</button>
-      <input ref={ numberRef } onMouseUp={ handleChangeNumber} type="range" min="0" max="100" />
-      {/* <button onClick={ handleChangeNumber }>change frequency increment value</button> */}
+      <button id="button" onClick={playSynth}>
+        click for synth note
+      </button>
+      <button onClick={resetFreqs}>reset</button>
+      <input
+        ref={numberRef}
+        onMouseUp={handleChangeNumber}
+        type="range"
+        min="0"
+        max="100"
+      />
     </div>
   );
 }
